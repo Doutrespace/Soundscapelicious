4962837b403cd3f90853e3a343511d7f3b94490e
Doutrespace <nils.karges@uni-wuerzburg.de>
2022-08-04 12:24:07 +0200

    changes

---
+++ pythonex.R
@@ -0,0 +1,17 @@
+#use python in shiny
+library(reticulate)
+conda_create("r-scrublet")
+conda_install(envname="r-scrublet", packages ="numpy","pip","git")
+conda_python(envname =  "r-scrublet")
+#use miniconda
+
+use_miniconda("r-scrublet", required=T)
+
+os <- import("os")
+os$system("pip list")
+
+#C:\\Users\\nik43jm\\Documents\\PhD\\Sience\\SoundscapeR\\SoundscapeR\py\\
+plotting <- import("plotting.py")
+
+
+reticulate::use_miniconda('soundscaper', required = TRUE)
