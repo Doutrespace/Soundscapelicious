4150f7e8859014f4afc85def3d31b9a8469fe712
Doutrespace <nils.karges@uni-wuerzburg.de>
2022-12-20 15:59:46 +0100

    changes in plotting

--- Circumplex.R
+++ Circumplex.R
@@ -1,31 +1,45 @@
-# Install and load the plotrix package
-#install.packages("plotrix")
-#remotes::install_github("ricardo-bion/ggradar")
-#devtools::install_github("jtlandis/ggside")
-library(ggplot2)
-library(ggside)
-library(tidyverse)
-library(tidyquant)
-library(devtools)
-library(plotrix)
-library(readr)
-library(tidyverse)
-library(ggraph)
-library(circumplex)
-library(ggradar)
-library(palmerpenguins)
-library(scales)
-library(showtext)
-library(fmsb)
-library(RColorBrewer)
-library(dplyr)
-library(plotly)
-library(ggExtra)
-library(hrbrthemes)
-library(ggside)
+#############################################################################################################
+######################################## OBJECTIVE ##########################################################
+#############################################################################################################
+#This notebook provides examples for analysing and visualising soundscape assessment data from the 
+#International Soundscape Database (ISD). The custom functions created for this purpose are stored 
+#in the circumplex.r file.
+# 
+#############################################################################################################
+######################################## LIBRARIES ##########################################################
+#############################################################################################################
+ipak <- function(pkg){
+  
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  
+  if (length(new.pkg)) 
+    
+    install.packages(new.pkg, dependencies = TRUE)
+  
+  sapply(pkg, require, character.only = TRUE)
+  
+}
 
+packages <- c("ggplot2","ggside","tidyverse","tidyquant","plotrix","sp","sf","readr","ggplot2","ggraph",
+              "circumplex","ggradar","ggdark","palmerpenguins","RColorBrewer","scales", "showtext","fmsb",
+              "dplyr","plotly","ggExtra", "hrbrthemes","ggside")
 
+# Load libraries
+ipak(packages)
 
+#############################################################################################################
+##################################### FOLDER CREATION #######################################################
+#############################################################################################################
+### Choose Main Folder
+Main_Fo <- rchoose.dir()
+
+### Change working Directory
+setwd(Main_Fo)
+
+###########################################################################################
+################################## DUMMY DATA #############################################
+###########################################################################################
+ 
 #sample data
 sample_dict <- list(
   RecordID = c("EX1", "EX2"),
@@ -46,22 +60,28 @@ sample_df_long <- pivot_longer(sample_dict_df, cols = -RecordID, names_to = "Att
 
 sample_df_long <- gather(sample_dict_df, key = "attribute", value = "rating", -RecordID)
 
-##############################################################################################
 
-#Circumplex plot
-
-# Create data: note in High school for Jonathan:
+#sample data spider plot
+# Create data
 set.seed(99)
 data <- as.data.frame(matrix( sample( 0:10 , 24 , replace=T) , ncol=8))
 colnames(data) <- c("pleasant" , "vibrant" , "eventful" , "chaotic" , "annoying", "monotonous" , "uneventful" , "calm")
 rownames(data) <- paste("Ext" , letters[1:3], sep="-")
 
+#load test data for scatterplots
+ssid  <- read_csv("C:/Users/nik43jm/Documents/PhD/Sience/Dissertation/SoundscapeR/data/TestDataOnly_2021-01-07.csv")
 
-# To use the fmsb package, I have to add 2 lines to the dataframe: the max and min of each topic to show on the plot!
+# Remove all rows with NA values
+#ssid <- ssid %>% na.omit()
+
+###########################################################################################
+################### Soundscape Circumplex Spyder PLOT  ####################################
+###########################################################################################
+
+
+# To use the fmsb package, I have to add 2 lines to the dataframe of max and min
 data <- rbind(rep(5,8) , rep(0,8) , data)
-
-# Check your data, it has to look like this!
-head(data)
+#head(data)
 
 # Set graphic colors
 coul <- brewer.pal(3, "Pastel2")
@@ -81,32 +101,42 @@ radarchart( data[-c(1,2),]  , axistype=0 , maxmin=F,
 # Add a legend
 legend(x=0.9, y=1.2, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=0.8, pt.cex=1)
 
-##########################################################################################################################################
-#load data
-ssid  <- read_csv("~/PhD/Sience/CodingIdeas/Soundscapy/test/test_DB/TestDataOnly_2021-01-07.csv")
-
-# Remove all rows with NA values
-ssid <- ssid %>% na.omit()
-
+###########################################################################################
+################### Soundscape  Loudness PLOT  ############################################
+###########################################################################################
 
 #set a theme
 theme_set(theme_bw())
 
+# Scatterplot
+gg <- ggplot(ssid, aes(x=Pleasant, y=Eventful)) + 
+  geom_point(aes(col=LocationID, size=loudness)) + 
+  geom_smooth(method="loess", se=F) + 
+  xlim(c(0, 1)) + 
+  ylim(c(0, 1)) + 
+  labs(subtitle="Perception vs Loudness", 
+       y="Eventful", 
+       x="Pleasant", 
+       title="Comparison of the soundscapes of urban spaces")
+
+plot(gg)
 
 
-ssid %>% 
-  ggplot(aes(x=Pleasant, y=Eventful, color=LocationID, size= loudness)) +
-  geom_point(alpha=0.7)
- 
-# Dummy data
-data <- data.frame(
-  var1 = rnorm(1000),
-  var2 = rnorm(1000, mean=2)
-)
+#new theme (because its nice)
+theme_set(theme_classic())
 
+# Plot
+g <- ggplot(ssid, aes(pleasant))
+g + geom_density(aes(fill=factor(LocationID)), alpha=0.8) + 
+  labs(title="Comparison of the soundscapes of urban spaces", 
+       subtitle="City Mileage Grouped by Number of cylinders",
+       caption="questionaire",
+       x="pleasentness",
+       fill="Location")
 
-
-
+###########################################################################################
+################### Soundscape Circumplex Spyder PLOT  ####################################
+###########################################################################################
 #First, rather than calculating the median response to each PA in the location, then calculating the circumplex coordinates,
 #the coordinates for each individual response are calculated. This results in a vector of ISOPleasant, ISOEventful values which are continuous variables 
 #from -1 to +1 and can be analysed statistically by calculating summary statistics (mean, standard deviation, quintiles, etc.) 
@@ -114,7 +144,7 @@ data <- data.frame(
 #This also enables each individual's response to be placed within the pleasant-eventful space. All of the responses for a location can then be plotted, 
 #giving an overall scatter plot for a location, as demonstrated in (i).
 
-  # Bin size control + color palette
+# circumplex mono
 ggplot(ssid, aes(x = Eventful, y = Pleasant) ) +
   geom_point() +
   stat_density_2d(aes(fill = ..level.., alpha=.6), geom = "polygon")+
@@ -156,20 +186,7 @@ ggplot(ssid, aes(x = Eventful, y = Pleasant) ) +
     alpha=0.6
   )
 
-
-#plotting the loudness
-# Chart
-p <- ggplot(ssid, aes(x=x) ) +
-  # Top
-  stat_density( aes(x = Eventful, y = ..density..), fill="#69b3a2",alpha=.6 ) +
-  geom_label( aes(x=1, y=1, label="Eventful"), color="#69b3a2") +
-  # Bottom
-  stat_density( aes(x = Pleasant, y = -..density..), fill= "#404080", alpha=.6) +
-  geom_label( aes(x=1, y=-1, label="Pleasant"), color="#404080") +
-  theme_ipsum()
-
-p
-
+# circumplex multi
 #Fig demonstrates how this simplified representation makes it possible to compare the soundscape of several locations in a sophisticated way.
 
 ggplot(ssid, aes(Eventful, Pleasant, colour=LocationID, fill=LocationID)) + 
